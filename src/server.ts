48c8b443f6c2687b2821a1f8529fae1bb7432b20
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2020-09-29 22:57:35 -0300

    configurando primeiras rotas

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
+import { ForeCastController } from './controllers/forecast';
+
 
 export class SetupServer extends Server {
     constructor(private port = 3000) {
@@ -10,13 +12,17 @@ export class SetupServer extends Server {
     // o método de baixo é privado, então tem que ser criado uma função get para ter acesso a ele.
     public init(): void {
         this.setupExpress();
+        this.setupControllers();
     }
-
-
 
     // pegar o que vier do body e fazer um parse. a ideia é conseguir analisar o que vier do body e converter para json.
     private setupExpress(): void {
         this.app.use(bodyParser.json());
+    }
+
+    private setupControllers(): void {
+        const forecastController = new ForeCastController;
+        this.addControllers([forecastController]);
     }
 
 }
